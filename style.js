ec82bfd001f266e54428d47dd5412e029fc4a5f3
NikxPurgar <nik.purgar10@gmail.com>
2025-12-13 18:12:03 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,11 @@
+function copy_ime() {
+  navigator.clipboard.writeText("Nik Purgar");
+}
+
+function copy_st() {
+  navigator.clipboard.writeText("031 613 220");
+}
+
+function copy_mail() {
+  navigator.clipboard.writeText("myvibe2007@gmail.com");
+}
